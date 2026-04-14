7d6e558b3e03664d32b0bedda327ba83cf63aa62
chethans2005 <chetansoyal@gmail.com>
2026-04-14 14:33:02 +0530

    initial '

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import StudioPage from './pages/StudioPage'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/studio" element={<StudioPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: '#2b2623',
+        beige: '#efe6db',
+        soft: '#d9cfc4'
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif']
+      }
+    }
+  },
+  plugins: []
+}
